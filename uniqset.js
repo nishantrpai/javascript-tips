4f714ad3c40b35bbf226e2dc13a2fdceafb8ef91
nishantrpai <eiglesias914@gmail.com>
2019-09-11 10:51:53 +0530

    getting a unique array from an array

---
+++ uniqset.js
@@ -0,0 +1,11 @@
+//Say you have an array
+let arr = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5];
+
+//The Set object lets you store unique values of any type.
+let set = new Set(arr)
+
+//Change from Set -> Array
+let uniqArr = Array.from(set)
+
+//If you log the unique array
+console.log(uniqArr);
