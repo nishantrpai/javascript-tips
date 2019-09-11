9f2773ee8782f10019c7afe9658e69799549706f
nishantrpai <eiglesias914@gmail.com>
2019-09-11 10:55:32 +0530

    updated the statements

--- uniqset.js
+++ uniqset.js
@@ -2,10 +2,10 @@
 let arr = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5];
 
 //The Set object lets you store unique values of any type.
-let set = new Set(arr)
+let set = new Set(arr) //Set { 1, 2, 3, 4, 5 }
 
 //Change from Set -> Array
 let uniqArr = Array.from(set)
 
 //If you log the unique array
-console.log(uniqArr);
+console.log(uniqArr); //[ 1, 2, 3, 4, 5 ]
